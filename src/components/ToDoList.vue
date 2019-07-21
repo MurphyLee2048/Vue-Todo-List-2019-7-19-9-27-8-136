9dd430faf1a04e19d03ee1339a593915bfd00ce0
murphylee2048 <1134697131@qq.com>
2019-07-21 19:56:07 +0800

    feat: Show Todo List

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Jquery To Do List</h1>
+    <i>Simple Todo List with adding and filter by diff status.</i>
+    <input type="text" v-model="list"><button v-on:click="add">Add</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ToDoList"
+  }
+</script>
+
+<style scoped>
+
+</style>
